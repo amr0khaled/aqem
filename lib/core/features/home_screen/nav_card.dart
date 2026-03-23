17fed2ec85c447426a788fb90da390dad5dd5bad
hh11909 <hamo1190927@gmail.com>
2026-03-23 07:05:39 +0200

    Fix 'BoxConstraints forces an infinite width.' Error: by editing NavCard:
    	-The problem happened when make a NavCard into a row.
    	-The reason is that the card wants to take all of the width of
    	the row because it has width of infinity.
    	-The solution is to wrap the card in NavCard Widget into
    	Expanded Widget.

--- lib/core/features/home_screen/nav_card.dart
+++ lib/core/features/home_screen/nav_card.dart
@@ -13,67 +13,69 @@ class NavCard extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20)
-      ),
+    return Expanded(
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20)
+        ),
 
-      child: InkWell(
-        onTap: (){
-          onCardTap();
-        },
-        hoverDuration: Duration(milliseconds: 90),
-        borderRadius: BorderRadius.circular(20),
-        child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return Padding(
-                padding: EdgeInsets.only(left: 20,top:(constraints.maxWidth < (MediaQuery.sizeOf(context).width/2))? 20:4,right: 20,bottom:4),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
+        child: InkWell(
+          onTap: (){
+            onCardTap();
+          },
+          hoverDuration: Duration(milliseconds: 90),
+          borderRadius: BorderRadius.circular(20),
+          child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Padding(
+                  padding: EdgeInsets.only(left: 20,top:(constraints.maxWidth < (MediaQuery.sizeOf(context).width/2))? 20:4,right: 20,bottom:4),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
 
-                  children: [
-                    if(constraints.maxWidth < (MediaQuery.sizeOf(context).width/2))
-                      SpecialIcon(
-                        icon: Icon(iconData,size: 28,color: iconDataColor??Colors.white,),
-                        color: iconBackgroundColor,
-                      )
-                    ,
-                    ListTile(
-                      trailing:
-                      constraints.maxWidth < (MediaQuery.sizeOf(context).width/2)?null:
-                      SpecialIcon(
-                        icon: Icon(iconData,size: 28,color: iconDataColor??Colors.white,),
-                        color: iconBackgroundColor,
-                      ),
-                      minVerticalPadding: 16,
-                      contentPadding: EdgeInsets.all(0),
-                      title: Text(
-                        title,
-                        textDirection: TextDirection.rtl,
-                        style: TextStyle(
-                          fontFamily: 'Kitab',//todo*************
-                          fontSize: 18.0,
-                          fontWeight: FontWeight(500),
+                    children: [
+                      if(constraints.maxWidth < (MediaQuery.sizeOf(context).width/2))
+                        SpecialIcon(
+                          icon: Icon(iconData,size: 28,color: iconDataColor??Colors.white,),
+                          color: iconBackgroundColor,
+                        )
+                      ,
+                      ListTile(
+                        trailing:
+                        constraints.maxWidth < (MediaQuery.sizeOf(context).width/2)?null:
+                        SpecialIcon(
+                          icon: Icon(iconData,size: 28,color: iconDataColor??Colors.white,),
+                          color: iconBackgroundColor,
                         ),
-                      ),
-                      subtitle: Text(
-                          subtitle,
+                        minVerticalPadding: 16,
+                        contentPadding: EdgeInsets.all(0),
+                        title: Text(
+                          title,
                           textDirection: TextDirection.rtl,
                           style: TextStyle(
-                              fontFamily: 'Kitab',//todo**************
-                              fontSize: 12.0,
-                              fontWeight: FontWeight(400),
-                              color: Color.fromRGBO(107, 107, 107, 1)
-                          )
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }
-          ),
+                            fontFamily: 'Kitab',//todo*************
+                            fontSize: 18.0,
+                            fontWeight: FontWeight(500),
+                          ),
+                        ),
+                        subtitle: Text(
+                            subtitle,
+                            textDirection: TextDirection.rtl,
+                            style: TextStyle(
+                                fontFamily: 'Kitab',//todo**************
+                                fontSize: 12.0,
+                                fontWeight: FontWeight(400),
+                                color: Color.fromRGBO(107, 107, 107, 1)
+                            )
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
+            ),
+        ),
       ),
     );
   }
