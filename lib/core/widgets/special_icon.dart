de1ef1620d1b04ab645b4e8bd118a2a18aaa2725
hh11909 <hamo1190927@gmail.com>
2026-03-23 09:43:16 +0200

    Edit the SpecialIcon:
    	- make it more flexible to apply any widget as its content.
    	-add to additional arguments:
    	 -this.gradientFlag=true: to enable or disable the color gradient
    	 effect.
    	 -this.shadowFlag=true: to enable or disable the back shadow
    	 effect.

--- lib/core/widgets/special_icon.dart
+++ lib/core/widgets/special_icon.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SpecialIcon extends StatelessWidget {
-  SpecialIcon({super.key,required this.icon,this.padding=14,this.borderRadius=24, this.color});
-  final Icon icon;
+  const SpecialIcon({super.key,required this.content,this.padding=14,this.borderRadius=24, this.color,this.gradientFlag=true,this.shadowFlag=true});
+  final Widget content;
   final double padding, borderRadius;
   final Color? color;
+  final bool gradientFlag,shadowFlag;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,13 +15,13 @@ class SpecialIcon extends StatelessWidget {
           gradient: LinearGradient(
               colors: [color??Theme.of(context).colorScheme.inversePrimary,
                 Color.from(alpha:1,
-                    red: (color?.r ?? Theme.of(context).colorScheme.inversePrimary.r)*0.9,
-                    green:  (color?.g ?? Theme.of(context).colorScheme.inversePrimary.g)*0.9,
-                    blue:  (color?.b ?? Theme.of(context).colorScheme.inversePrimary.b)*0.9
+                    red: (color?.r ?? Theme.of(context).colorScheme.inversePrimary.r)*(gradientFlag?0.9:1),
+                    green:  (color?.g ?? Theme.of(context).colorScheme.inversePrimary.g)*(gradientFlag?0.9:1),
+                    blue:  (color?.b ?? Theme.of(context).colorScheme.inversePrimary.b)*(gradientFlag?0.9:1)
                 )
               ]
           ),
-          boxShadow: [
+          boxShadow: shadowFlag?[
             BoxShadow(
                 color: Color.fromRGBO(0, 0, 0, 0.1),
                 blurRadius: 6,
@@ -33,9 +34,9 @@ class SpecialIcon extends StatelessWidget {
                 spreadRadius: -3,
                 offset: Offset(0, 10)
             ),
-          ]
+          ]:null
       ),
-      child: icon,
+      child: content,
     );
   }
 }
