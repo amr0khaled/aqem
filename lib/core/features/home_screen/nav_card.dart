75d4f8bbf2f1d1a2b3e708c0ab4317a29a9ea154
hh11909 <hamo1190927@gmail.com>
2026-03-23 06:24:41 +0200

    Edit: home_screen/NavCard:
     -add a ripple effect to nav cards by wrapping the child of card with
     InkWill widget.
     -fix the top padding size of card

--- lib/core/features/home_screen/nav_card.dart
+++ lib/core/features/home_screen/nav_card.dart
@@ -18,44 +18,51 @@ class NavCard extends StatelessWidget{
           borderRadius: BorderRadius.circular(20)
       ),
 
-      child: Padding(
-              padding: const EdgeInsets.only(left: 20,top: 4,right: 20,bottom:4),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-            SpecialIcon(
-              icon: Icon(iconData,size: 28,color: iconDataColor??Colors.white,),
-              color: iconBackgroundColor,
-            )
-                  ,
-                  ListTile(
-                    minVerticalPadding: 16,
-                    contentPadding: EdgeInsets.all(0),
-                    title: Text(
-                      title,
-                      textDirection: TextDirection.rtl,
-                      style: TextStyle(
-                        fontFamily: 'Kitab',//todo*************
-                        fontSize: 18.0,
-                        fontWeight: FontWeight(500),
-                      ),
-                    ),
-                    subtitle: Text(
-                        subtitle,
+      child: InkWell(
+        onTap: (){
+          onCardTap();
+        },
+        hoverDuration: Duration(milliseconds: 90),
+        borderRadius: BorderRadius.circular(20),
+        child: Padding(
+                padding: const EdgeInsets.only(left: 20,top: 20,right: 20,bottom:4),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+              SpecialIcon(
+                icon: Icon(iconData,size: 28,color: iconDataColor??Colors.white,),
+                color: iconBackgroundColor,
+              )
+                    ,
+                    ListTile(
+                      minVerticalPadding: 16,
+                      contentPadding: EdgeInsets.all(0),
+                      title: Text(
+                        title,
                         textDirection: TextDirection.rtl,
                         style: TextStyle(
-                            fontFamily: 'Kitab',//todo**************
-                            fontSize: 12.0,
-                            fontWeight: FontWeight(400),
-                            color: Color.fromRGBO(107, 107, 107, 1)
-                        )
-                    ),
-                  )
-                ],
-              ),
-        ),
+                          fontFamily: 'Kitab',//todo*************
+                          fontSize: 18.0,
+                          fontWeight: FontWeight(500),
+                        ),
+                      ),
+                      subtitle: Text(
+                          subtitle,
+                          textDirection: TextDirection.rtl,
+                          style: TextStyle(
+                              fontFamily: 'Kitab',//todo**************
+                              fontSize: 12.0,
+                              fontWeight: FontWeight(400),
+                              color: Color.fromRGBO(107, 107, 107, 1)
+                          )
+                      ),
+                    )
+                  ],
+                ),
+          ),
+      ),
     );
   }
 }
