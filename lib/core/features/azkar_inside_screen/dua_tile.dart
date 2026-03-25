b6dd17952b7966483d8857922fc1d2cccdf6cec1
hh11909 <hamo1190927@gmail.com>
2026-03-25 11:09:23 +0200

    Edit DuaTile:
    	- unwrap the expand widget from card widget

--- lib/core/features/azkar_inside_screen/dua_tile.dart
+++ lib/core/features/azkar_inside_screen/dua_tile.dart
@@ -13,108 +13,89 @@ class _DuaTileState extends State<DuaTile> {
   int _counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-        child: Card(
-          shadowColor: Colors.transparent,
-          shape:RoundedRectangleBorder(
-              borderRadius: .circular(20)
-          ),
-          child: InkWell(
-            borderRadius: .circular(20),
-            hoverDuration: Duration(milliseconds: 90),
-            onTap: (){
-              setState(() {
-                _expand=!_expand;
-              });
-            },
-            child: Directionality(
-              textDirection: .rtl,
-              child: ListTile(
-                contentPadding: EdgeInsets.all(12),
-                titleAlignment: .top,
-                horizontalTitleGap: 12,
-                leading: SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: TextButton(
-                      onPressed: (){
-                        setState(() {
-                          _counter++;
-                        });
-                      },
-                      onLongPress: (){
-                        setState(() {
-                          _counter=0;
-                        });
-                      },
-                      style: TextButton.styleFrom(
-                          shape: CircleBorder(),
-                          padding: .zero,
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.orange,
-                      ),
-                      child: Text(_counter.toString(),
-                        style: TextStyle(
-                          fontWeight: .new(400),
-                          fontSize: 16
-                        ),)
+    return Card(
+      shadowColor: Colors.transparent,
+      shape:RoundedRectangleBorder(
+          borderRadius: .circular(20)
+      ),
+      child: InkWell(
+        borderRadius: .circular(20),
+        hoverDuration: Duration(milliseconds: 90),
+        onTap: (){
+          setState(() {
+            _expand=!_expand;
+          });
+        },
+        child: Directionality(
+          textDirection: .rtl,
+          child: ListTile(
+            contentPadding: EdgeInsets.all(12),
+            titleAlignment: .top,
+            horizontalTitleGap: 12,
+            leading: SizedBox(
+              height: 40,
+              width: 40,
+              child: TextButton(
+                  onPressed: (){
+                    setState(() {
+                      _counter++;
+                    });
+                  },
+                  onLongPress: (){
+                    setState(() {
+                      _counter=0;
+                    });
+                  },
+                  style: TextButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: .zero,
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.orange,
                   ),
-                ),
-                title: Text(
-                  maxLines: _expand?null:2,
-                  overflow: _expand?null:.ellipsis,
-                  widget.dua??"أَصْبَحْنَا وَأَصْبَحَ الْمُلْكُ لِلَّهِ، وَالْحَمْدُ لِلَّهِ، لَا إِلَهَ إِلَّا اللَّهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرٌ، رَبِّ أَسْأَلُكَ خَيْرَ مَا فِي هَذَا الْيَوْمِ وَخَيْرَ مَا بَعْدَهُ، وَأَعُوذُ بِكَ مِنْ شَرِّ مَا فِي هَذَا الْيَوْمِ وَشَرِّ مَا بَعْدَهُ",
-                    style: const TextStyle(
-                      fontFamily: 'Kitab',
-                      height: 1.62
-                    )),
-                subtitle: Row(
+                  child: Text(_counter.toString(),
+                    style: TextStyle(
+                      fontWeight: .new(400),
+                      fontSize: 16
+                    ),)
+              ),
+            ),
+            title: Text(
+              maxLines: _expand?null:2,
+              overflow: _expand?null:.ellipsis,
+              widget.dua??"أَصْبَحْنَا وَأَصْبَحَ الْمُلْكُ لِلَّهِ، وَالْحَمْدُ لِلَّهِ، لَا إِلَهَ إِلَّا اللَّهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرٌ، رَبِّ أَسْأَلُكَ خَيْرَ مَا فِي هَذَا الْيَوْمِ وَخَيْرَ مَا بَعْدَهُ، وَأَعُوذُ بِكَ مِنْ شَرِّ مَا فِي هَذَا الْيَوْمِ وَشَرِّ مَا بَعْدَهُ",
+                style: const TextStyle(
+                  fontFamily: 'Kitab',
+                  height: 1.62
+                )),
+            subtitle: Row(
 
+              spacing: 8,
+              children: [
+                Column(
+                  mainAxisSize: .min,
+                  crossAxisAlignment: .start,
                   spacing: 8,
                   children: [
-                    Column(
-                      mainAxisSize: .min,
-                      crossAxisAlignment: .start,
-                      spacing: 8,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 2.0,horizontal: 8),
-                          decoration: BoxDecoration(
-                            color: .fromRGBO(212-10, 175-10, 55-10,1),//todo*******************
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Text(
-                            widget.duaNarrator??"رواه مسلم",
-                            style: TextStyle(
-                              fontFamily: 'Kitab',
-                            ),
-                          ),
-                        ),
-                        if(widget.duaNotice!=null&&widget.duaNotice!.length>19)Container(
-                          padding: const .symmetric(vertical: 2.0,horizontal: 8),
-                          decoration: BoxDecoration(
-                            border: .all(
-                                color: .fromRGBO(13, 126, 94, 0.12)
-                            ),
-                            borderRadius: .circular(12),
-                          ),
-                          child: Text(
-                            widget.duaNotice!,
-                            style: TextStyle(
-                              fontFamily: 'Kitab',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    if(widget.duaNotice!=null && widget.duaNotice!.length<19)Container(
+                    Container(
                       padding: const EdgeInsets.symmetric(vertical: 2.0,horizontal: 8),
                       decoration: BoxDecoration(
-                        border: .all(
-                          color: Color.fromRGBO(13, 126, 94, 0.12)
+                        color: .fromRGBO(212-10, 175-10, 55-10,1),//todo*******************
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Text(
+                        widget.duaNarrator??"رواه مسلم",
+                        style: TextStyle(
+                          fontFamily: 'Kitab',
                         ),
-                          borderRadius: .circular(12),
+                      ),
+                    ),
+                    if(widget.duaNotice!=null&&widget.duaNotice!.length>19)Container(
+                      padding: const .symmetric(vertical: 2.0,horizontal: 8),
+                      decoration: BoxDecoration(
+                        border: .all(
+                            color: .fromRGBO(13, 126, 94, 0.12)
+                        ),
+                        borderRadius: .circular(12),
                       ),
                       child: Text(
                         widget.duaNotice!,
@@ -125,24 +106,41 @@ class _DuaTileState extends State<DuaTile> {
                     ),
                   ],
                 ),
-                trailing: IconButton(
-                  highlightColor: Colors.transparent,
-                    onPressed: (){
-                      setState(() {
-                        _favourite=!_favourite;
-                      });
-                    },
-                    icon: Icon(
-                        _favourite?Icons.favorite:Icons.favorite_outline,
-                        color: _favourite?Colors.red:null,
+
+                if(widget.duaNotice!=null && widget.duaNotice!.length<19)Container(
+                  padding: const EdgeInsets.symmetric(vertical: 2.0,horizontal: 8),
+                  decoration: BoxDecoration(
+                    border: .all(
+                      color: Color.fromRGBO(13, 126, 94, 0.12)
                     ),
-                        iconSize: 16,
-                        padding: .zero,
+                      borderRadius: .circular(12),
+                  ),
+                  child: Text(
+                    widget.duaNotice!,
+                    style: TextStyle(
+                      fontFamily: 'Kitab',
+                    ),
+                  ),
                 ),
-              ),
+              ],
+            ),
+            trailing: IconButton(
+              highlightColor: Colors.transparent,
+                onPressed: (){
+                  setState(() {
+                    _favourite=!_favourite;
+                  });
+                },
+                icon: Icon(
+                    _favourite?Icons.favorite:Icons.favorite_outline,
+                    color: _favourite?Colors.red:null,
+                ),
+                    iconSize: 16,
+                    padding: .zero,
             ),
           ),
-        )
+        ),
+      ),
     );
   }
 }
