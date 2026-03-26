f78d2037844786c1d86f00312c20c62d7d8d33e5
hh11909 <hamo1190927@gmail.com>
2026-03-26 12:46:41 +0200

    Edit SpecialIcon:
    	-make the gradient color from top center to bottom center.
    	-add new named parameter called scale so you can scale the icon
    	with the same ratios.

--- lib/core/widgets/special_icon.dart
+++ lib/core/widgets/special_icon.dart
@@ -1,42 +1,47 @@
 import 'package:flutter/material.dart';
 
 class SpecialIcon extends StatelessWidget {
-  const SpecialIcon({super.key,required this.content,this.padding=14,this.borderRadius=24, this.color,this.gradientFlag=true,this.shadowFlag=true});
+  const SpecialIcon({super.key,required this.content,this.padding=14,this.borderRadius=24, this.color,this.gradientFlag=true,this.shadowFlag=true,this.scale=1.0});
   final Widget content;
-  final double padding, borderRadius;
+  final double padding, borderRadius,scale;
   final Color? color;
   final bool gradientFlag,shadowFlag;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all((padding)),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(borderRadius),
-          gradient: LinearGradient(
-              colors: [color??Theme.of(context).colorScheme.inversePrimary,
-                Color.from(alpha:1,
-                    red: (color?.r ?? Theme.of(context).colorScheme.inversePrimary.r)*(gradientFlag?0.9:1),
-                    green:  (color?.g ?? Theme.of(context).colorScheme.inversePrimary.g)*(gradientFlag?0.9:1),
-                    blue:  (color?.b ?? Theme.of(context).colorScheme.inversePrimary.b)*(gradientFlag?0.9:1)
-                )
-              ]
-          ),
-          boxShadow: shadowFlag?[
-            BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.1),
-                blurRadius: 6,
-                spreadRadius: -4,
-                offset: Offset(0, 4)
+    return Transform.scale(
+      scale: scale,
+      child: Container(
+        padding: EdgeInsets.all((padding)),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius),
+            gradient: LinearGradient(
+                colors: [color??Theme.of(context).colorScheme.inversePrimary,
+                  Color.from(alpha:1,
+                      red: (color?.r ?? Theme.of(context).colorScheme.inversePrimary.r)*(gradientFlag?0.9:1),
+                      green:  (color?.g ?? Theme.of(context).colorScheme.inversePrimary.g)*(gradientFlag?0.9:1),
+                      blue:  (color?.b ?? Theme.of(context).colorScheme.inversePrimary.b)*(gradientFlag?0.9:1)
+                  )
+                ],
+              begin: .topCenter,
+              end: .bottomCenter
             ),
-            BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.1),
-                blurRadius: 15,
-                spreadRadius: -3,
-                offset: Offset(0, 10)
-            ),
-          ]:null
+            boxShadow: shadowFlag?[
+              BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.1),
+                  blurRadius: 6,
+                  spreadRadius: -4,
+                  offset: Offset(0, 4)
+              ),
+              BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.1),
+                  blurRadius: 15,
+                  spreadRadius: -3,
+                  offset: Offset(0, 10)
+              ),
+            ]:null
+        ),
+        child: content,
       ),
-      child: content,
     );
   }
 }
