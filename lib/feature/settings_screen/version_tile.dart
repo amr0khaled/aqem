6087c7f4c9cb278d23cae1c0caea0a8b89f90796
hh11909 <hamo1190927@gmail.com>
2026-03-28 06:11:05 +0200

    Create VersionTile, which:
                -is StatelessWidget.
                -is used to view the version number.

---
+++ lib/feature/settings_screen/version_tile.dart
@@ -0,0 +1,30 @@
+import 'package:aqem/feature/settings_screen/settings_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class VersionTile extends StatelessWidget {
+  const VersionTile({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SettingsListTile(
+        title: 'الإصدار',
+        subTitle: 'إصدار التطبيق الحالي',
+        iconData: Icons.info_outline,
+        trailing: Container(
+          padding: .only(top: 4,right: 12,left: 12,bottom: 3),
+          decoration: BoxDecoration(
+            color: .fromRGBO(157, 157, 157, 0.15),
+            borderRadius: .circular(16),
+          ),
+          child: Text(
+            '1.0.0',//todo
+            style: TextStyle(
+              fontSize: 14,
+              fontFamily: 'kitab',
+              color: Color(0xFF6B6B6B),
+
+            ),
+          ),
+        ),
+        onTap: (){});
+  }
+}
