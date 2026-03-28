e30c214e233eabd3a54b7c048b85b182d1edfe89
hh11909 <hamo1190927@gmail.com>
2026-03-28 19:06:42 +0200

    Create SettingsCard widget which:
    	-represents the blueprint of the main sections in
    	settings-screen, which contain specific SettingsListTiles for
    	each section.

---
+++ lib/feature/settings_screen/settings_card.dart
@@ -0,0 +1,59 @@
+import 'package:aqem/feature/settings_screen/settings_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class SettingsCard extends StatelessWidget {
+  const SettingsCard({super.key,required this.title,required this.settingsCardList});
+  final String title;
+  final List<Widget> settingsCardList;
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: .rtl,
+      child: Column(
+        spacing: 13,
+        crossAxisAlignment: .start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 18),
+            child: Text(title,
+            style: TextStyle(
+              fontFamily: 'kitab',
+              fontSize: 12,
+              color: .new(0xFF6B6B6B),
+              fontWeight: .new(500)
+            ),),
+          ),
+          Card(
+            margin: .zero,
+            clipBehavior: .antiAlias,
+            shape: RoundedRectangleBorder(
+                borderRadius: .circular(20)
+            ),
+            child: Column(
+              mainAxisSize: .min,
+              children: [
+                ...settingsCardList.map((tile){
+                return   Column(
+                  children: [
+                    tile,
+                    if(settingsCardList.last!=tile)
+                    Divider(
+                      color: Color.from(
+                          red:Theme.of(context).colorScheme.inversePrimary.r  ,
+                          green:Theme.of(context).colorScheme.inversePrimary.g ,
+                          blue: Theme.of(context).colorScheme.inversePrimary.b ,
+                          alpha: 0.1),
+                      height: 0,
+                      indent: 15,
+                    )
+                  ],
+                );
+                }),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
