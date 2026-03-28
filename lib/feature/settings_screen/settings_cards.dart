23dee83114b3a7e2d7b16102532e654928445e21
hh11909 <hamo1190927@gmail.com>
2026-03-28 19:42:47 +0200

    Create SettingsCards Widget,which:
    	-is the result view 'in settings_screen' of the combination of all SettingsListTiles created.

---
+++ lib/feature/settings_screen/settings_cards.dart
@@ -0,0 +1,41 @@
+import 'package:aqem/feature/settings_screen/about_app_tile.dart';
+import 'package:aqem/feature/settings_screen/dark_mode_tile.dart';
+import 'package:aqem/feature/settings_screen/drawing_type_tile.dart';
+import 'package:aqem/feature/settings_screen/font_size_tile.dart';
+import 'package:aqem/feature/settings_screen/language_tile.dart';
+import 'package:aqem/feature/settings_screen/notification_tile.dart';
+import 'package:aqem/feature/settings_screen/settings_card.dart';
+import 'package:aqem/feature/settings_screen/version_tile.dart';
+import 'package:flutter/material.dart';
+
+class SettingsCards extends StatelessWidget {
+  const SettingsCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: ListView(
+        children: [
+          SettingsCard(
+              title: 'الإعدادات العامة',
+              settingsCardList: [DarkModeTile(),LanguageTile(),NotificationTile()]
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SettingsCard(
+              title: 'إعدادات القراءة',
+              settingsCardList: [DrawingTypeTile(),FontSizeTile()]),
+          SizedBox(
+            height: 20,
+          ),
+          SettingsCard(
+              title: 'حول التطبيق',
+              settingsCardList: [AboutAppTile(),VersionTile()]),
+        ],
+
+      ),
+    );
+  }
+}
