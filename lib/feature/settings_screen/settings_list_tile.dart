0f5f99eb8ebdfa9b4d81ba643a8a1afd811b9b26
hh11909 <hamo1190927@gmail.com>
2026-03-29 09:45:15 +0200

    Edit SettingsListTile:
            - reduce the right padding (-15).
            - centralize the _secondaryTextColor and make its alpha 8F.
            - unwrap Directionality from it.

--- lib/feature/settings_screen/settings_list_tile.dart
+++ lib/feature/settings_screen/settings_list_tile.dart
@@ -7,64 +7,63 @@ class SettingsListTile extends StatelessWidget {
   final String? trailingTitle;
   final IconData iconData;
   final Widget? trailing;
+  final Color _secondaryTextColor = const Color(0x8F6B6B6B);
   final void Function() onTap;
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: .rtl,
-      child: InkWell(
-        onTap: onTap,
-        child: ListTile(
-          horizontalTitleGap: 12,
-          // tileColor: Colors.orange,//////////////////////////////todo
-          leading: SpecialIcon(
-            content: Icon(iconData,
-              color: Theme.of(context).colorScheme.inversePrimary,
-              size: 20,
-            ),
-            padding: 10,
-            borderRadius: 40,
-            gradient: LinearGradient(
-              colors: [
-              Color.fromRGBO(13, 126, 94, 0.1),
-              Color.fromRGBO(212, 175, 55, 0.1)
-            ],
-              begin: .topCenter,
-              end: .bottomCenter,
-            ),
-            shadowFlag: false,
+    return InkWell(
+      onTap: onTap,
+      child: ListTile(
+        horizontalTitleGap: 12,
+        // tileColor: Colors.orange,//////////////////////////////todo
+        leading: SpecialIcon(
+          content: Icon(iconData,
+            color: Theme.of(context).colorScheme.inversePrimary,
+            size: 20,
+          ),
+          padding: 10,
+          borderRadius: 40,
+          gradient: LinearGradient(
+            colors: [
+            Color.fromRGBO(13, 126, 94, 0.1),
+            Color.fromRGBO(212, 175, 55, 0.1)
+          ],
+            begin: .topCenter,
+            end: .bottomCenter,
+          ),
+          shadowFlag: false,
 
-          ),
-          title: Text(title,
-            style: TextStyle(fontFamily: 'kitab'),
-          ),
-          subtitle: Text(subTitle,
-              style: TextStyle(fontFamily: 'kitab',fontSize: 12,color: Color(0xFF6B6B6B))),
-          trailing: trailing??SizedBox(
-            width: 80,
-            child: Row(
-              mainAxisAlignment: .end,
-              spacing: 12,
-              children: [
-                Text(trailingTitle??'',
-                  style: const TextStyle(
-                      fontSize: 14,
-                      fontFamily: 'kitab',
-                      color: .new(0xFF6B6B6B)
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5,horizontal: 7.5),
-                  child: Icon(
-                    Icons.arrow_forward_ios,
-                    size: 14,
-                  ),
-                )
-              ],
-            ),
-          ),
-          contentPadding: .only(top: 0,bottom: 0,left: 16,right: 16+15),
         ),
+        title: Text(title,
+          style: TextStyle(fontFamily: 'kitab'),
+        ),
+        subtitle: Text(subTitle,
+            style: TextStyle(fontFamily: 'kitab',fontSize: 12,color: _secondaryTextColor)),
+        trailing: trailing??SizedBox(
+          width: 80,
+          child: Row(
+            mainAxisAlignment: .end,
+            spacing: 8,
+            children: [
+              Text(trailingTitle??'',
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: 'kitab',
+                    color: _secondaryTextColor
+                ),
+              ),
+               Padding(
+                padding: EdgeInsets.symmetric(vertical: 5,horizontal: 7.5),
+                child: Icon(
+                  Icons.arrow_forward_ios,
+                  color: _secondaryTextColor,
+                  size: 14,
+                ),
+              )
+            ],
+          ),
+        ),
+        contentPadding: .only(top: 0,bottom: 0,left: 16,right: 16),
       ),
     );
   }
